aecccd8af0d64b844382ca19836f38f9786acdad
ngangavic <ngangavictor10@gmail.com>
2020-07-31 22:22:00 +0300

    bottom nav setup

--- app/src/main/java/com/ngangavictor/grocerystore/categories/CategoriesActivity.kt
+++ app/src/main/java/com/ngangavictor/grocerystore/categories/CategoriesActivity.kt
@@ -1,16 +1,20 @@
 package com.ngangavictor.grocerystore.categories
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.ngangavictor.grocerystore.R
 
@@ -22,11 +26,41 @@ class CategoriesActivity : AppCompatActivity() {
     private lateinit var imageViewNotifications: ImageView
     private lateinit var imageViewProfile: ImageView
 
+    private lateinit var imageViewSearch: ImageView
+    private lateinit var imageViewList: ImageView
+    private lateinit var imageViewBasket: ImageView
+
+    private lateinit var layoutSearch: ConstraintLayout
+    private lateinit var layoutList: ConstraintLayout
+    private lateinit var layoutCart: ConstraintLayout
+
+    private lateinit var floatingActionButtonCancelCart: FloatingActionButton
+    private lateinit var floatingActionButtonCancelList: FloatingActionButton
+    private lateinit var floatingActionButtonCancelSearch: FloatingActionButton
+
+    private lateinit var recyclerViewSearch: RecyclerView
+    private lateinit var recyclerViewList: RecyclerView
+    private lateinit var recyclerViewCart: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
-//        val toolbar: Toolbar = findViewById(R.id.toolbar)
-//        setSupportActionBar(toolbar)
+
+        imageViewSearch=findViewById(R.id.imageViewSearch)
+        imageViewList=findViewById(R.id.imageViewList)
+        imageViewBasket=findViewById(R.id.imageViewBasket)
+
+        layoutSearch=findViewById(R.id.layoutSearch)
+        layoutList=findViewById(R.id.layoutList)
+        layoutCart=findViewById(R.id.layoutCart)
+
+        floatingActionButtonCancelCart=findViewById(R.id.floatingActionButtonCancelCart)
+        floatingActionButtonCancelList=findViewById(R.id.floatingActionButtonCancelList)
+        floatingActionButtonCancelSearch=findViewById(R.id.floatingActionButtonCancelSearch)
+
+        recyclerViewSearch=findViewById(R.id.recyclerViewSearch)
+        recyclerViewList=findViewById(R.id.recyclerViewList)
+        recyclerViewCart=findViewById(R.id.recyclerViewCart)
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
@@ -59,6 +93,48 @@ class CategoriesActivity : AppCompatActivity() {
             }
         }
 
+        clickListeners()
+
+    }
+
+    private fun clickListeners(){
+        imageViewSearch.setOnClickListener {
+            layoutSearch.visibility=View.VISIBLE
+            layoutList.visibility=View.GONE
+            layoutCart.visibility=View.GONE
+            recyclerViewSearch.visibility=View.GONE
+            floatingActionButtonCancelSearch.visibility=View.GONE
+        }
+
+        imageViewList.setOnClickListener {
+            layoutSearch.visibility=View.GONE
+            layoutList.visibility=View.VISIBLE
+            layoutCart.visibility=View.GONE
+        }
+
+        imageViewBasket.setOnClickListener {
+            layoutSearch.visibility=View.GONE
+            layoutList.visibility=View.GONE
+            layoutCart.visibility=View.VISIBLE
+        }
+
+        floatingActionButtonCancelCart.setOnClickListener {
+            layoutSearch.visibility=View.GONE
+            layoutList.visibility=View.GONE
+            layoutCart.visibility=View.GONE
+        }
+
+        floatingActionButtonCancelList.setOnClickListener {
+            layoutSearch.visibility=View.GONE
+            layoutList.visibility=View.GONE
+            layoutCart.visibility=View.GONE
+        }
+
+        floatingActionButtonCancelSearch.setOnClickListener {
+            layoutSearch.visibility=View.GONE
+            layoutList.visibility=View.GONE
+            layoutCart.visibility=View.GONE
+        }
     }
 
 //    override fun onCreateOptionsMenu(menu: Menu): Boolean {
