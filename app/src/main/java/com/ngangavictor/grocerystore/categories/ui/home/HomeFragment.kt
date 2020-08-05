630a09206afcfba690922d5139bb5023e56fc05c
ngangavic <ngangavictor10@gmail.com>
2020-08-05 12:14:06 +0300

    category set up

--- app/src/main/java/com/ngangavictor/grocerystore/categories/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ngangavictor/grocerystore/categories/ui/home/HomeFragment.kt
@@ -8,11 +8,22 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ngangavictor.grocerystore.R
+import com.ngangavictor.grocerystore.adpters.CategoryAdapter
+import com.ngangavictor.grocerystore.models.CategoryModel
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+
+    private lateinit var categoryAdapter: CategoryAdapter
+
+    private lateinit var categoryList: MutableList<CategoryModel>
+
+    private lateinit var recyclerViewFruits:RecyclerView
+    private lateinit var root:View
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,11 +32,25 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_home)
-//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
+        root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        categoryList = ArrayList()
+        recyclerViewFruits=root.findViewById(R.id.recyclerViewFruits)
+        recyclerViewFruits.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewFruits.setHasFixedSize(true)
+        homeViewModel.getProducts().observe(viewLifecycleOwner, Observer { it->
+            categoryList = it as MutableList<CategoryModel>
+
+            categoryAdapter = CategoryAdapter(
+                categoryList as ArrayList<CategoryModel>
+            )
+
+            categoryAdapter.notifyDataSetChanged()
+
+            recyclerViewFruits.adapter = categoryAdapter
+
+        })
+
         return root
     }
 }
--- app/src/main/java/com/ngangavictor/grocerystore/categories/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ngangavictor/grocerystore/categories/ui/home/HomeViewModel.kt
@@ -1,13 +1,39 @@
 package com.ngangavictor.grocerystore.categories.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.ngangavictor.grocerystore.models.CategoryModel
+import com.ngangavictor.grocerystore.repos.FirebaseRepository
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    var firebaseRepository = FirebaseRepository()
+
+    private val products = MutableLiveData<List<CategoryModel>>()
+
+    fun getProducts(): LiveData<List<CategoryModel>> {
+        firebaseRepository.getProducts().addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.e("data",snapshot.toString())
+                var productList : MutableList<CategoryModel> = mutableListOf()
+                for (data in snapshot.children){
+                    productList.add(CategoryModel(data.child("image").value.toString(),data.child("name").value.toString(),data.child("desc").value.toString(),data.child("price").value.toString(),data.child("quantity").value.toString(),data.child("category").value.toString()))
+                }
+                products.value=productList
+            }
+
+        })
+
+        return products
     }
-    val text: LiveData<String> = _text
+
 }
