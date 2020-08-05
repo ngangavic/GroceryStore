c02dcd6230b0cb8d6e6e6f09a9620fe37531ddfc
ngangavic <ngangavictor10@gmail.com>
2020-08-05 12:13:30 +0300

    firebase repo

---
+++ app/src/main/java/com/ngangavictor/grocerystore/repos/FirebaseRepository.kt
@@ -0,0 +1,17 @@
+package com.ngangavictor.grocerystore.repos
+
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class FirebaseRepository {
+
+     private val database: FirebaseDatabase=Firebase.database
+
+
+    fun getProducts():DatabaseReference{
+      return database.getReference("green-orchard").child("products")
+    }
+
+}
