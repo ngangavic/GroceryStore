fd2021d1d30e105c2dab8aadff17bbf28d9a10bc
ngangavic <ngangavictor10@gmail.com>
2020-08-05 12:13:01 +0300

    category holder

---
+++ app/src/main/java/com/ngangavictor/grocerystore/holders/CategoryHolder.kt
@@ -0,0 +1,17 @@
+package com.ngangavictor.grocerystore.holders
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ngangavictor.grocerystore.R
+
+class CategoryHolder(itemView: View):
+    RecyclerView.ViewHolder(itemView){
+
+    val imageViewProduct:ImageView=itemView.findViewById(R.id.imageViewProduct)
+    val textViewProductName:TextView=itemView.findViewById(R.id.textViewProductName)
+    val textViewProductDescription:TextView=itemView.findViewById(R.id.textViewProductDescription)
+    val textViewProductPrice:TextView=itemView.findViewById(R.id.textViewProductPrice)
+
+}
