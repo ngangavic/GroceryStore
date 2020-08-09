57cfad6b2bd605bd70240a6005f540ddb0a0a876
ngangavic <ngangavictor10@gmail.com>
2020-08-09 10:47:28 +0300

    categories adapter

---
+++ app/src/main/java/com/ngangavictor/grocerystore/adpters/CategoriesAdapter.kt
@@ -0,0 +1,48 @@
+package com.ngangavictor.grocerystore.adpters
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.RecyclerView
+import com.ngangavictor.grocerystore.R
+import com.ngangavictor.grocerystore.categories.CategoriesActivity
+import com.ngangavictor.grocerystore.categories.ui.category.CategoryFragment
+import com.ngangavictor.grocerystore.holders.CategoryHolder
+import com.ngangavictor.grocerystore.models.CategoryModel
+import com.squareup.picasso.Picasso
+
+class CategoriesAdapter(private val context: Context,private val products: ArrayList<CategoryModel>) :
+    RecyclerView.Adapter<CategoryHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryHolder {
+        val viewHolder: CategoryHolder?
+        val layoutView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_row_categories, parent, false)
+        viewHolder = CategoryHolder(layoutView)
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: CategoryHolder, position: Int) {
+        holder.textViewProductDescription.text = products[position].productDesc
+        holder.textViewProductName.text = products[position].productName
+        holder.textViewProductPrice.text = "KES." + products[position].productPrice
+        Picasso.get().load(products[position].productImage)
+            .placeholder(R.drawable.loading)
+            .into(holder.imageViewProduct)
+        holder.imageViewProduct.setOnClickListener {
+            val activity=context as CategoriesActivity
+           val categoryFragment=CategoryFragment.newInstance(products[position].category)
+            val fragmentTransaction = activity.supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.nav_host_fragment, categoryFragment)
+            fragmentTransaction.commit()
+        }
+    }
+}
