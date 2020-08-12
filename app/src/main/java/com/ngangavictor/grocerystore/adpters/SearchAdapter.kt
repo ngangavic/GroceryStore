4a118620fc7305d3a4e04e595180ea68e75a6a3c
ngangavic <ngangavictor10@gmail.com>
2020-08-12 16:09:42 +0300

    add search items to cart

--- app/src/main/java/com/ngangavictor/grocerystore/adpters/SearchAdapter.kt
+++ app/src/main/java/com/ngangavictor/grocerystore/adpters/SearchAdapter.kt
@@ -1,15 +1,20 @@
 package com.ngangavictor.grocerystore.adpters
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 import com.ngangavictor.grocerystore.R
+import com.ngangavictor.grocerystore.categories.CategoriesActivity
+import com.ngangavictor.grocerystore.models.Cart
 import com.ngangavictor.grocerystore.models.CategoryModel
 import com.squareup.picasso.Callback
 import com.squareup.picasso.NetworkPolicy
@@ -27,6 +32,7 @@ class SearchAdapter(val context: Context, private val productList: ArrayList<Cat
         val textViewProductDescription =
             itemView.findViewById<TextView>(R.id.textViewProductDescription)
         val textViewProductPrice = itemView.findViewById<TextView>(R.id.textViewProductPrice)
+        val buttonAddCart = itemView.findViewById<Button>(R.id.buttonAddCart)
 
         Picasso.get().load(productList[position].productImage).placeholder(R.drawable.loading)
             .networkPolicy(
@@ -48,6 +54,27 @@ class SearchAdapter(val context: Context, private val productList: ArrayList<Cat
         textViewProductName.text = productList[position].productName
         textViewProductDescription.text = productList[position].productDesc
         textViewProductPrice.text = "KES." + productList[position].productPrice
+
+        buttonAddCart.setOnClickListener {
+            val ac = context as Activity
+
+            CategoriesActivity.cartViewModel.addCartItem(
+                Cart(
+                    productList[position].key,
+                    productList[position].productName,
+                    productList[position].productDesc,
+                    productList[position].productPrice,
+                    1,
+                    productList[position].productImage,
+                    productList[position].productPrice
+                )
+            )
+            Snackbar.make(
+                ac.findViewById(android.R.id.content),
+                "Added to cart",
+                Snackbar.LENGTH_SHORT
+            ).show()
+        }
 
         return itemView
     }
